d25b3b6686ca3ac02d65ee761c9c3c017aa4bbd0
GURMAN1912 <manansinghsambhi@gmail.com>
2024-03-16 23:50:53 +0530

    Settings page

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "../layouts/dashboard";
 
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
+import Setting from "../pages/dashboard/Setting";
 
 const Loadable = (Component) => (props) => {
   return (
@@ -24,6 +25,7 @@ export default function Router() {
       children: [
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
         { path: "app", element: <GeneralApp /> },
+        { path: "setting", element: <Settings /> },
         
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
@@ -35,5 +37,8 @@ export default function Router() {
 //dynamic loading using lazy
 const GeneralApp = Loadable(
   lazy(() => import("../pages/dashboard/GeneralApp")),
+);
+const Settings = Loadable(
+  lazy(() => import("../pages/dashboard/Setting")),
 );
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
